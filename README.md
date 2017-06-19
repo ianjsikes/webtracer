68aedcede59bf8ba97b0a0428cfe54cc57d63da6
Ian J Sikes <ianjsikes@gmail.com>
2017-06-19 12:26:56 -0700

    Load wasm module in index.js

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 > An interactive raytracer built using Elm, Rust, and WebAssembly 🎉
 
 This project is bootstrapped with [Create Elm App](https://github.com/halfzebra/create-elm-app).
+
+## Notes
+To compile the Rust project:
+1. `rustup override set nightly`
+2. `make`
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import './main.css'
 const logoPath = require('./logo.svg')
 const Elm = require('./App.elm')
+const wasm = require('./wasm.js')
 
 const root = document.getElementById('root')
 
 Elm.App.embed(root, logoPath)
+
+const Module = {
+  wasmBinaryFile: require('./wasm/hello.wasm'),
+  ENVIRONMENT: 'WEB',
+}
+
+wasm(Module)
+
+// require('./wasm/hello.js');
